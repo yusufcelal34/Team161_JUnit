cd79aafd418dde8c9c28b394be9b7b79f81bab2e
Yusuf Celal <yusufcelal34@gmail.com>
2025-05-28 20:49:56 +0300

    28.05.2025-20:50

--- src/test/java/tests/day16_webtables_excelOtomasyon/C03_ReadExcel.java
+++ src/test/java/tests/day16_webtables_excelOtomasyon/C03_ReadExcel.java
@@ -32,7 +32,7 @@ public class C03_ReadExcel {
         String expectedCellValue = "Başkent (İngilizce)";
         String actualCellValue = satir1Hucre2;
 
-        Assertions.assertEquals(expectedCellValue, actualCellValue);
+        Assertions.assertEquals(expectedCellValue,actualCellValue);
 
         //- 2.satir 4.cell’in afganistan’in baskenti “Kabil” oldugunu test edin
 
@@ -41,7 +41,7 @@ public class C03_ReadExcel {
                 .getCell(3)
                 .getStringCellValue();
 
-        Assertions.assertEquals(expectedCellValue, actualCellValue);
+        Assertions.assertEquals(expectedCellValue,actualCellValue);
 
 
         //- Ulke sayisinin 190 oldugunu test edin
@@ -49,33 +49,54 @@ public class C03_ReadExcel {
         // index kullandigi icin satir sayisini bulmak istersek 1 eklemeliyiz 190+1 ==> 191 satir var
 
         int expectedUlkeSayisi = 190;
-        int actualUlkeSayisi = sayfa1.getLastRowNum() + 1 - 1;
+        int actualUlkeSayisi = sayfa1.getLastRowNum() + 1  -1 ;
         // +1 index sayisindan satir sayisini bulabilmek icin
         // -1 excel'deki ilk satir ulke ismi degil baslik
 
-        Assertions.assertEquals(expectedUlkeSayisi, actualUlkeSayisi);
+        Assertions.assertEquals(expectedUlkeSayisi,actualUlkeSayisi);
 
+
+        //- Fiziki olarak kullanilan satir sayisinin 191 oldugunu test edin
         int expectedKullanilanSatirSayisi = 191;
         int actualKullanilanSatirSayisi = sayfa1.getPhysicalNumberOfRows();
 
-        //- Fiziki olarak kullanilan satir sayisinin 191 oldugunu test edin
-        Assertions.assertEquals(expectedKullanilanSatirSayisi, actualKullanilanSatirSayisi);
+        Assertions.assertEquals(expectedKullanilanSatirSayisi,actualKullanilanSatirSayisi);
 
         //- Ingilizce ismi Netherland olan ulkenin baskentinin turkce Amsterdam oldugunu test edin
 
-        for (int i = 0; i < sayfa1.getLastRowNum(); i++) {
+        for (int i = 0; i <= sayfa1.getLastRowNum() ; i++) {
+
             String satirdakiIngUlkeIsmi = sayfa1.getRow(i)
                     .getCell(0)
                     .getStringCellValue();
             String satirdakiTurkceBaskentIsmi = sayfa1.getRow(i)
                     .getCell(3)
                     .getStringCellValue();
-            if (satirdakiIngUlkeIsmi.equals("Netherland")) {
-                Assertions.assertEquals("Amsterdam", satirdakiTurkceBaskentIsmi);
+
+            if (satirdakiIngUlkeIsmi.equalsIgnoreCase("Netherlands")){
+                Assertions.assertEquals(satirdakiTurkceBaskentIsmi,"Amsterdam");
             }
+
         }
+
         //- Turkce baskent isimlerinde Ankara bulundugunu test edin
 
+        boolean ankaraVarMi = false;
+
+        for (int i = 0; i <=sayfa1.getLastRowNum() ; i++) {
+            String satirdakiTurkceBaskentIsmi = sayfa1.getRow(i)
+                    .getCell(3)
+                    .getStringCellValue();
+
+            if (satirdakiTurkceBaskentIsmi.equalsIgnoreCase("Ankara")){
+                ankaraVarMi = true;
+                break;
+            }
+        }
+
+        Assertions.assertEquals(ankaraVarMi,true);
+
+        Assertions.assertTrue(ankaraVarMi);
 
     }
 }
--- src/test/java/tests/day17_excelAutomation_getScreenshoot/C01_ExceldeKullanilmayanSatirlar.java
+++ src/test/java/tests/day17_excelAutomation_getScreenshoot/C01_ExceldeKullanilmayanSatirlar.java
@@ -15,35 +15,48 @@ public class C01_ExceldeKullanilmayanSatirlar {
     @Test
     public void test01() throws IOException {
 
-
-// ulkeler excel'inde Sayfa2'ye gidin
+        // ulkeler excel'inde Sayfa2'ye gidin
         String dosyaYolu = "src/test/java/tests/day16_webtables_excelOtomasyon/ulkeler.xlsx";
         FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
         Workbook workbook = WorkbookFactory.create(fileInputStream);
         Sheet sayfa2 = workbook.getSheet("Sayfa2");
 
-// kullanilan en son satirin 20.satir oldugunu test edin
-        int expectedSonSatirIndexi = 19;
+        // kullanilan en son satirin 20.satir oldugunu test edin
+        int expectedSonSatirIndexi = 19 ;
         int actualSonSatirIndexi = sayfa2.getLastRowNum();
-        Assertions.assertEquals(expectedSonSatirIndexi, actualSonSatirIndexi);
 
-// reel olarak data yazilan satir sayisinin 8 oldugunu test edin
-        int expectedSatirSayisi = 8;
-        int actualSatirSayisi = sayfa2.getPhysicalNumberOfRows();
-        Assertions.assertEquals(expectedSatirSayisi, actualSatirSayisi);
+        Assertions.assertEquals(expectedSonSatirIndexi,actualSonSatirIndexi);
 
-// 4.satir, 1.sutun'daki sayinin 3 oldugunu test edin
+        // reel olarak data yazilan satir sayisinin 8 oldugunu test edin
+        int expectedKullanilanSatirSayisi = 8;
+        int actualKullanilanSatirSayisi = sayfa2.getPhysicalNumberOfRows();
+
+        Assertions.assertEquals(expectedKullanilanSatirSayisi,actualKullanilanSatirSayisi);
+
+        // 4.satir, 1.sutun'daki sayinin 3 oldugunu test edin
+        // kullanilan hucre
         double expectedDeger = 3;
         double actualDeger = sayfa2.getRow(3).getCell(0).getNumericCellValue();
-        Assertions.assertEquals(expectedDeger, actualDeger);
 
-// 4.satir, 2.sutun'daki datayi yazdirin
-        System.out.println(sayfa2.getRow(3).getCell(1));
-
-// 5.satir, 3.sutun'daki datayi yazdirin
-        System.out.println(sayfa2.getRow(4).getCell(2));
+        Assertions.assertEquals(expectedDeger,actualDeger);
 
 
+        // 4.satir, 2.sutun'daki datayi yazdirin
+        // kullanilan satirdaki kullanilmayan hucre
+
+        System.out.println( sayfa2.getRow(3).getCell(1)  ); // null
+
+        // System.out.println( sayfa2.getRow(3).getCell(1).getStringCellValue()  ); // NullPointerException
+
+        // System.out.println( sayfa2.getRow(3).getCell(1).getNumericCellValue()  ); // NullPointerException
+
+
+        // 5.satir, 3.sutun'daki datayi yazdirin
+        // kullanilmayan satirdaki kullanilmayan hucre
+
+        // System.out.println( sayfa2.getRow(4).getCell(2) ); // NullPointerException
+        // sayfa2.getRow(4) ==> satir bos oldugu icin null olarak isaretlenir
+        // null bir deger kullanilmaya kalkisilirsa NullPointerException
 
 
     }
